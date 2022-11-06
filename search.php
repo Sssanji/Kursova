ff89e30f04c1795d32d3bb65c202158dc16ea491
Ivanenko_Bogdan <90037657+Sssanji@users.noreply.github.com>
2022-11-06 20:04:26 +0200

    Add files via upload

--- search.php
+++ search.php
@@ -8,7 +8,7 @@ if (!$_SESSION['user']){
 
 
 require_once "vendor/connect.php";
-
+require_once "vendor/search.php";
 $category = mysqli_query ($connect, "SELECT * FROM `categories`");
 $category = mysqli_fetch_all ($category);
 $products = mysqli_query ($connect, "SELECT * FROM `products`");
@@ -18,7 +18,7 @@ $products = mysqli_fetch_all ($products);
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Main</title>
+    <title>Search</title>
     <link rel='stylesheet' type='text/css' href='style/style.css'>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -30,14 +30,27 @@ $products = mysqli_fetch_all ($products);
 
 
   <div class='menu'>
-      <a href='#'>Головна</a>
+      <a href='main.php'>Головна</a>
       <a href='add.php'>Додати</a>
       <a href='search.php'>Пошук</a>
       <a href='#'>пап</a>
       <a href="vendor/exit.php" style='color: red;'>Вихід</a>
   </div>
 
-<div class='table'>
+
+
+     <div class='sosi'>
+    <form method = 'post'>
+       
+        <h1>Пошук товару</h1>
+        <input type="text" name="search"> 
+        <input type="submit" value="Пошук" name = "searchB">
+     </form>
+    </div>
+
+
+
+    <div class='table'>
 <table>
     <tr>
       <th>id</th>
@@ -51,29 +64,23 @@ $products = mysqli_fetch_all ($products);
     </tr>
 
     <?php
-    foreach($products as $product) {
+    foreach($row as $resulte) {
         ?>
           <tr>
-            <td><?= $product[0] ?></td>
-            <td><?= $product[1] ?></td>
-            <td><?= $product[2] ?></td>
-            <td><?= $product[3] ?></td> 
-            <td><?= $product[4] ?></td> 
-            <td><?= $product[5] ?></td> 
-            <td><a href="update.php?id=<?= $product[0] ?>">Оновити</a></td>
-            <td><a href="vendor/delete.php?id=<?= $product[0] ?>">Видалити</a></td>
+            <td><?= $resulte[0] ?></td>
+            <td><?= $resulte[1] ?></td>
+            <td><?= $resulte[2] ?></td>
+            <td><?= $resulte[3] ?></td> 
+            <td><?= $resulte[4] ?></td> 
+            <td><?= $resulte[5] ?></td> 
+            <td><a href="update.php?id=<?= $resulte[0] ?>">Оновити</a></td>
+            <td><a href="vendor/delete.php?id=<?= $resulte[0] ?>">Видалити</a></td>
           </tr>
         <?php
       }
        ?>
   </table>
     </div>
-  <a href="vendor/exit.php">Вихід</a>
-  <a href="export.php">Експорт</a>
 
-        
-
- 
-    
-</body>
-</html>
+ </body>
+ </html>
--- vendor/search.php
+++ vendor/search.php
@@ -4,7 +4,13 @@ require_once 'connect.php';
 if ($_POST['searchB']){
     $inputSearch = $_POST['search'];
     $result = mysqli_query ($connect, "SELECT * FROM `products` WHERE `title` = '$inputSearch'");
-    $row = mysqli_fetch_assoc($result);
-    $resulte = ("<h1>".$row['title']."</h1><p>".$row['description']."</p><p>".$row['price']."</p><br>");
+    $row = mysqli_fetch_all($result);
+    
+    // $resulte = ("<h1>".$row['title']."</h1><p>".$row['description']."</p><p>".$row['price']."</p><br>");
+//     $resulte = (
+//     "<h1>".$row['title']."</h1><p>".$row['description']."</p><p>".$row['price']."</p><br>"
+
+
+//     );
 }
 ?>
